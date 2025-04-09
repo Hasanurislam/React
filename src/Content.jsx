96ce7e93a6d64e06972163ce956aafa36032c508
hasanurislam <hasanur7811@gmail.com>
2025-04-09 20:54:13 +0530

    first commit

---
+++ src/Content.jsx
@@ -0,0 +1,18 @@
+const Content=()=>{
+    return   <div className="hero-content">
+    <h1>YOUR FEET DESERVE THE BEST</h1>
+    <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET    DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+    <div className='btns'>
+      <button className='btn2'>SHOP NOW</button>
+      <button className='btn2 btn3'>CATEGORY</button>
+    </div>
+  
+    <div className='img'>
+    <p>Also Available on </p>
+    <img src='./public/flipkart.png' alt='Flipkart'></img>
+    <img src='./public/amazon.png' alt='Amazon'></img>
+    </div>
+  </div>
+}
+
+export default Content;
---
+++ src/Product.jsx
@@ -0,0 +1,6 @@
+const Product=()=>{
+    return  <div className="hero-section">
+    <img src='./public/shoe_image.png' alt='Shoe'></img>
+  </div>
+}
+export default Product;
